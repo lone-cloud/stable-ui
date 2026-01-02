d0f25e9f9adf42a23ea900b15e9f2076df0348a6
Concedo <39025047+LostRuins@users.noreply.github.com>
2026-01-02 18:10:47 +0800

    added window close protection

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from "./router";
 
 import 'element-plus/theme-chalk/dark/css-vars.css'
 import "./assets/main.css";
+import { useGeneratorStore } from "./stores/generator";
 
 const app = createApp(App);
 
@@ -14,3 +15,12 @@ app.use(router);
 
 app.mount("#app");
 router.replace("/");
+
+window.addEventListener("beforeunload", (event) => {
+    let busy = useGeneratorStore().generating;
+    if(busy)
+    {
+        event.preventDefault();
+        event.returnValue = "";
+    }
+});
